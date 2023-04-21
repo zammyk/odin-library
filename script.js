5ea3b059b5048b4acde40b73a35961a3f973ed3b
Rahul Hembrom <69256240+zammyk@users.noreply.github.com>
2023-04-21 19:52:01 +0530

    Add functionality to display and submit form

--- script.js
+++ script.js
@@ -11,3 +11,28 @@ function addBookToLibrary(title, author, pages, read) {
   const newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
 }
+
+const form = document.getElementById("book-form");
+const inputBookName = document.getElementById("book-name");
+const inputAuthor = document.getElementById("author");
+const inputPages = document.getElementById("pages");
+const inputBookRead = document.getElementById("book-read");
+const addBookButton = document.getElementById("add-book");
+
+addBookButton.addEventListener("click", () => {
+  form.classList.add("active");
+});
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let bookName = inputBookName.value;
+  let author = inputAuthor.value;
+  let pages = inputPages.value;
+  let bookRead = inputBookRead.value;
+  if (bookName === "" || author === "" || pages === "" || bookRead === "") {
+    console.log("gottem");
+  } else {
+    addBookToLibrary(bookName, author, pages, bookRead);
+    form.classList.remove("active");
+  }
+});
